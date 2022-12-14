2fc101d7face68033ec5a2a9ec10350587acd772
Nishant Racherla <nishant.racherla@gmail.com>
2022-12-14 13:50:09 -0800

    finished phase 3

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/context/ClimateContext.js
@@ -1,2 +1,22 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+import { createContext, useContext, useState } from 'react';
+
+export const ClimateContext = createContext();
+
+export const ClimateProvider = ({ children }) => {
+  const [temperature, setTemperature] = useState(50);
+  
+  return (
+    <ClimateContext.Provider
+      value={{
+        temperature,
+        setTemperature
+      }}
+    >
+      {children}
+    </ClimateContext.Provider>
+  )
+};
+
+export const useClimateContext = () => useContext(ClimateContext);
