baf79ec467b68c62e707d15f6f3fc3dc2967edbd
Nishant Racherla <nishant.racherla@gmail.com>
2022-12-14 12:05:35 -0800

    finished context review project

--- practice-for-week-14-react-context-review-with-tests-long-practice/src/App.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice/src/App.js
@@ -1,10 +1,12 @@
 import SelectedCoffeeBean from "./components/SelectedCoffeeBean";
+import SetCoffeeBean from "./components/SetCoffeeBean";
 import coffeeBeans from "./mockData/coffeeBeans.json";
 
 function App() {
   return (
     <>
       <SelectedCoffeeBean />
+      <SetCoffeeBean coffeeBeans={coffeeBeans} />
     </>
   );
 }
